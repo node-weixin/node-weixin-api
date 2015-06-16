7327f372309fcafeaf169a2ad2ceb35272371591
calidion <calidion@gmail.com>
2015-06-16 20:27:55 +0800

    enabled tokenzie

--- test/units/apis.spec.js
+++ test/units/apis.spec.js
@@ -4,12 +4,13 @@ var weixin = require('./../../index');
 
 var validator = require('validator');
 
-
+var access_token = null;
 describe("Weixin apis Test", function () {
   it('should be able to get a token', function(done) {
     weixin.api.tokenize(function(error, json) {
       assert(true, !!json.access_token);
       assert(true, validator.isNumeric(json.expire_in));
+      access_token = json.access_token;
       done();
     });
   });
