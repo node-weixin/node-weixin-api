48ce2a63fe8ca6e1c9d996a5cd6e3952b8816788
calidion <calidion@gmail.com>
2015-06-17 18:39:37 +0800

    enabled restful api

--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -1,5 +1,5 @@
 describe("Begin NodeWeixinApi testing", function () {
-  var units = require('./units/auth.spec');
-  var units = require('./units/apis.spec');
+  var auth = require('./units/auth.spec');
+  var apis = require('./units/apis.spec');
 });
 
