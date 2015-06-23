9c23f51077aadbbd4a365085603922f50e96f876
calidion <calidion@gmail.com>
2015-06-23 11:26:49 +0800

    add test for callbacks

--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -1,5 +1,6 @@
 describe("Begin NodeWeixinApi testing", function () {
   require('./units/auth.spec');
   require('./units/apis.spec');
+  require('./units/callbacks.spec');
 });
 
---
+++ test/units/callbacks.spec.js
@@ -0,0 +1,61 @@
+var request = require('supertest');
+var assert = require("assert");
+var config = require('./../config');
+var weixin = require('./../../index');
+var express = require('express');
+var bodyParser = require('body-parser')
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+app.post('/weixin', function(req, res){
+  weixin.api.auth.ack(req, res);
+});
+
+weixin.auth.init(config);
+
+describe('Weixin callbacks\' tests', function () {
+
+  it('should be able to auth weixin signature', function (done) {
+
+    var token = config.appToken;
+    var time = new Date().getTime();
+    var nonce = 'nonce';
+    var echostr = 'Hello world!';
+    var signature = weixin.auth.generateSignature(token, time, nonce);
+    var data = {
+      signature: signature,
+      timestamp: time,
+      nonce: nonce,
+      echostr: echostr
+    };
+    request(app)
+      .post('/weixin')
+      .send(data)
+      .expect(200)
+      .expect(echostr)
+      .end(done);
+  });
+
+  it('should be failed to auth weixin signature', function (done) {
+
+    var token = config.appToken;
+    var time = new Date().getTime();
+    var nonce = 'nonce';
+    var echostr = 'Hello world!';
+    var signature = weixin.auth.generateSignature(token, time, nonce);
+    var data = {
+      signature: signature,
+      timestamp: time,
+      nonce: nonce
+    };
+    request(app)
+      .post('/weixin')
+      .send(data)
+      .expect(200)
+      .expect('Input Invalid!')
+      .end(done);
+  });
+});
